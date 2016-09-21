67a93994f907475cdc95737a49787f6a7fc19acf
kister19 <kister19@up.edu>
2016-09-21 15:49:40 -0700

    The UPPER button is now operational.

--- app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -10,17 +10,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import java.util.ArrayList;
 
-public class TextModActivity extends ActionBarActivity {
+public class TextModActivity extends ActionBarActivity implements View.OnClickListener{
 
     // array-list that contains our images to display
     private ArrayList<Bitmap> images;
@@ -28,6 +31,9 @@ public class TextModActivity extends ActionBarActivity {
     // instance variables containing widgets
     private ImageView imageView; // the view that shows the image
 
+    //UpperCase button
+    private Button upperCase;
+    private EditText editText;
     /**
      * @see android.app.Activity#onCreate(android.os.Bundle)
      */
@@ -73,6 +79,9 @@ public class TextModActivity extends ActionBarActivity {
         // define a listener for the spinner
         spinner.setOnItemSelectedListener(new MySpinnerListener());
 
+        upperCase = (Button)findViewById(R.id.button6);
+        upperCase.setOnClickListener(this);
+        editText = (EditText)findViewById(R.id.editText);
     }
 
     /**
@@ -126,6 +135,17 @@ public class TextModActivity extends ActionBarActivity {
         @Override
         public void onNothingSelected(AdapterView<?> parentView) {
             // your code here
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Turns the edit text to upper case
+        if(v == upperCase){
+            Log.i("Getting text", ":)");
+            String text = editText.getText().toString();
+            text = text.toUpperCase();
+            editText.setText(text);
         }
     }
 }
